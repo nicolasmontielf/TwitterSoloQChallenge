40d1f0524d7032e97361cddf22a0cb1788a5e9fb
Nicolás Montiel <56762247+nicolasmontielf@users.noreply.github.com>
2021-04-17 11:13:30 -0400

    Lista actualizada de streamers

--- streamers.js
+++ streamers.js
@@ -1,100 +1,332 @@
 const streamers = [
-    {
-        "id": "n0rGlG6xW5YP3pJuILK5XhO7atMKM6Yp-BRTZl5ThDS1tRM",
-        "accountId": "qqfimy3pU2O05g2lahoUCkSyBGG8r29VSFvWMpAzY5SyFw",
-        "puuid": "R17AdR-val7TTimlgaZniCpaKrkoQ4m1x3cXa15ij9mbw-5t-x0TIlBLID3US_LYOWUpzI3LWmMeRg",
-        "name": "elmillor1",
-        "twitter": "@test",
-        "twitch": "https://www.twitch.tv/1"
-    },
-    {
-        "id": "fMSjFt57finp6QMUFgWcRWVSlaq0YMKHwkosN0-P1yFjjHw",
-        "accountId": "PLUeHIwiyf6V7K-4YF2LBYiN0MjcvP5bnG996-JR6bth2Q",
-        "puuid": "QXUAzOQdK6SohBqundHF1nCUT_XcxTfLXUFr_3VWzRdm8l5ZfVzy0A-TiDPrkDx2Zmu4TU926R7ngw",
-        "name": "Coinflip Pét",
-        "twitter": "@test",
-        "twitch": "https://www.twitch.tv/2"
-    },
-    {
-        "id": "vxmSL5Rb5l6M6msdOBFaWiByj20EPvK0gxbXEPjFpZRYU5Y",
-        "accountId": "1AclU6HY_OisjLW1HptYGLEL7m7zs7ufZcvFEmEWNhNqyxA",
-        "puuid": "e-aJEUN9sgx3IhfCBllYU9zegWvsuB48wqiwSHVcQRpT3OPFfIhwOU-ddClT_qCF5fWUNBhR2xTunA",
-        "name": "Sampyy",
-        "twitter": "@test",
-        "twitch": "https://www.twitch.tv/3"
-    },
-    {
-        "id": "PTdbCK5DU8f24P8mLgzK5xnuMckFVsWtXLyxoXR7_1dc2nk",
-        "accountId": "3EMFDmQCVqaipDdwh_lw-pLfkX3VXAsj8J-sTWGXjIu4jA",
-        "puuid": "4zap4rOTRLhneUmctPbsEyvGvckYR6StgvZlenbLq7QNMIR5fTiM2AYOqiIzdfkQebPYD6lXvC1SJw",
-        "name": "I use crutches",
-        "twitter": "@test",
-        "twitch": "https://www.twitch.tv/4"
-    },
-    {
-        "id": "d9_zSQ9ZT87seRW_kXlAvRFPUK9moe3xSAQ2Ow7yPztOONp8",
-        "accountId": "ch4YSCKMK_WDieGDFajnfDEb4vZ8bEwhAfNonz9PLaelhnZ3NUV2sceO",
-        "puuid": "Li0VEIAKU4p_0CzF2hYvYclm_UPwCQCzWot0TwgSnMebTpZ_L8sg5auCdJDWtG5TGaeqrf6Y3qLMGw",
-        "name": "MerciIess",
-        "twitter": "@test",
-        "twitch": "https://www.twitch.tv/5"
-    },
-    {
-        "id": "a0Z9heD3Tei3HtQAN361UBSFaq1WNbc5GGYGAYhkX0Pwn3U_",
-        "accountId": "7k6k50z6jWcITWAI91r9p_rXAYLwQ1ZtRQaaqJpdR1SrCkzhXv-LtjJ2",
-        "puuid": "_ouKhjmeWxEJGuDWsf-kra9kDD_lVfDDoXP-pwqf3OFMMpAEwpFUk41z67_H96sn9TABsy7dEqAZRA",
-        "name": "ZNY",
-        "twitter": "@test",
-        "twitch": "https://www.twitch.tv/6"
-    },
-    {
-        "id": "WMb052hOPZ3MPmX0sbAaCe5ruZsJb9-omIR2JI68eBvUyoFs",
-        "accountId": "PJ6LDdXxqjFxIMh8M5wyKXCp0AQkmQRGmnUiNwzubn3APww",
-        "puuid": "7uzyJ1k667otlzszIVs6mdk8omLWl5sm9VIVA-QEH21txjfc-hPPqIeALnYPx4vZn0knMT2Q0_771A",
-        "name": "E1waru",
-        "twitter": "@test",
-        "twitch": "https://www.twitch.tv/7"
-    },
-    {
-        "id": "8JS7iF5BWDQ24lzC5hIqbHzCBdpHIXHOiHcCtwy6X-mkx112",
-        "accountId": "CRhp0i3bSOrWiyv-uhYthH8_ISUjhPoDxylb4cS96UYjwMSRqe3UIusI",
-        "puuid": "7hSCSMvEz-pYGBjvhxcoKx9cSnfb5xvMfpuxFiapSWCvsO9-IX3sOGuEEX-0aatXiqBsHAAP6gbAow",
-        "name": "ElmiilloR NA",
-        "twitter": "@test",
-        "twitch": "https://www.twitch.tv/8"
-    },
-    {
-        "id": "7SggOYlr00xQ5g17ckvviH9rVZzxQXFUte2l9-sJ9jJ2Si6q",
-        "accountId": "FsOB75Q1DFfGxrNh_tmn_gPV3gNiSFUE9qFjAJI8HVgfJF0TWNXu7DDY",
-        "puuid": "HPc5sr9pXbiLVogXqsW0oM4j_fdez_UerFv3xwL2YGs9HpJ_f5-pMFuinf4ZSs1eZOyV_ezJhyD7gA",
-        "name": "Satoru Gοjο",
-        "twitter": "@test",
-        "twitch": "https://www.twitch.tv/9"
-    },
-    {
-        "id": "aYc5xm2vi8Onut2BdOoLT-0IpVIJVmvD3dZkU9YiaEwhKX3r",
-        "accountId": "Z4WoXPecGTEkvzkrd8ISQ28Dd53ndR6fdwZV1v4cCey6UwMzltjMM9QF",
-        "puuid": "iqJTaDg0UucEDc3pX3HiF8OdbdoYk1Ik2d3oTYaka4OBmVvIdxVjUaAJxavmVTQ7cX_OyoXxRJI3aQ",
-        "name": "Bet365 Smurf",
-        "twitter": "@test",
-        "twitch": "https://www.twitch.tv/021"
-    },
-    {
-        "id": "SKe4qEYnmlv7-e3q6u7H_h5oLmjDuLhCv9x_MZwP3AABmnE",
-        "accountId": "O5QKF1iMdFqfeOtHKdk-4nLTm2tSFHUPgoYjievWcqL8EQ",
-        "puuid": "VnIxdc_cnO-OUha8FU1lOFMG_Eo8eEyebYJvFK8sjWphil1wUOtl1I9-ZJUQRQRyeQNNqZA-yhhV8A",
-        "name": "TheGrefg",
-        "twitter": "@test",
-        "twitch": "https://www.twitch.tv/021"
-    },
-    {
-        "id": "On9NJUnc8Mf_yvlTnudDBACr1Iy7gihQg5y0EL8q-jhJF80",
-        "accountId": "9pDFMy-_uB9oNXmA_Fnd9SS5nbOl2T1SEcfLcLmKa7MCAw",
-        "puuid": "10ZQdqQD0P6hQ7SCifJiICjYeWW_mo5i8q6rcOq2ZGXP2EihHCf5U92QA55UOVilRs7FJMAbkrFoDA",
-        "name": "disYOx",
-        "twitter": "@test",
-        "twitch": "https://www.twitch.tv/021"
-     }
+{
+    id: '99T9-Bpd6tRsfT4CMOOCBjtzNkT08cQ51m1VbV2AXisMcRrAR30dg5qpjA',
+    accountId: 'IpR93ZaNrjp8XRRCPYt5iIMxDGGFHO8go8i0lgW3QAfC1jcUTEQ1aLwk',
+    puuid: 'NyViqZ4wIt8qhhD3kyjy0srkrouO30iBFmTz3k1mhCw1RgkV4CTkMkuAXDwiv_ro1Nb-v3NfDMbsFg',
+    name: 'soy murciANO',
+    twitter: "https://twitter.com/Koldo_LoL",
+    twitch: "https://www.twitch.tv/koldo_lol"
+},
+{
+    id: '2sm5EhbXWFVsm_6Qq41sjHdNoPMUZpMJFhKwpD9OZ_tl6Enu70IppD7BLA',
+    accountId: 'Ow3t7Ah5aCyT2t-h21JQ39fvUurxgja3Gpl5IS0wZLq-crckkFdRP8PR',
+    puuid: 'mWJE7rpiOPfz_dwHHSoJ2JlFgI0wPpNl_sz0b7jHFK0dEGvBs50mXZCzyjrc0omUDJS1Pyt-BmELlQ',
+    name: 'el tetoncito ',
+    twitter: "https://twitter.com/Flakked_LoL",
+    twitch: "https://www.twitch.tv/getflakked"
+},
+{
+    id: 'ylLgI6ETyfU_N4Pq1XH-5BSIQEsPGb-P_0qo2JEzu4cVKiXO5hV-XeZVog',
+    accountId: '5UR4Eg2kDfXswOkSwK9RC0FSklMv18pRtceq0VZteA9l5Viw8g0ht73X',
+    puuid: 'wZPnyIj84VOi5F7GKI3VjdvSC1RU2zzPnQVPzzwqBIE7SBJU47VVS4SIxtQRDEh3Y5OPM-_WL0BHXQ',
+    name: 'El Cabrornn',
+    twitter: "https://twitter.com/GGinyin",
+    twitch: "https://www.twitch.tv/ginyin"
+},
+{
+    id: 'YXptYPWR77PCHZ5-8IrS0mEFBQ0KLfn0Q6F4wK07y7mF2KNNvTbcy56_RQ',
+    accountId: 'r5akpzrp2PD0vM3nRrkKHR75hR09Opl5oMYuBcEzJrvjfPTVjSEU7z2h',
+    puuid: 'wQ5HfD51oCvMdfY51RpiQWsrs7fpGHtSqOPmMbku7Tv1_vg-ZhK8PiWCcf8SDLanmOT2-DE9_xIlGQ',
+    name: 'Look My Bubble',
+    twitter: "https://twitter.com/JavierrLoL",
+    twitch: "https://www.twitch.tv/JavierrLoL"
+},
+{
+    id: 'qHL1T3uG9_N4XkkjVNbj7-b94_FroE0f0MnQ4Dmtx-IGPrq03Z240_coYQ',
+    accountId: 'WiX3L5a_vO-JW1QLRNVa8f1iZVJb4MWdjcIUetFjajVOQcSbLUn2Cys5',
+    puuid: 'JhYeIf8Vhy04s8mEzqAOWeF29R3TxrX6hv277bcqazR1gEWo5hpMnIX5waTI0Sl4uwdBUQk68o9tvA',
+    name: 'CEO of Grupales',
+    twitter: "https://twitter.com/CarritosKami",
+    twitch: "https://www.twitch.tv/carritoskami"
+},
+{
+    id: 'ZZyKWuxYhOFr5cc2iLhNXT4B9O60cDMh9wAVV8BYFN0dJphKGFlTt9MNkQ',
+    accountId: 'f8aAWx2M6Wm_iVPNwKirc4DMXzwyfz7XXI3DooxTeGr8IC2hoGU383CM',
+    puuid: 'npvdLud3J3YNZbHayGak1tRRmun2bBnqTReQ2Fd_pEyLaw1M-2LPsaEtTXLPjDVWnNMKd7ZfhShFaw',
+    name: 'CEO de los Hunos',
+    twitter: "https://twitter.com/Attila",
+    twitch: "https://www.twitch.tv/Attila"
+},
+{
+    id: 'BikZFrxtt5dciJCnoop-D5YzgdiQOrTHzqARYrxKxlBPLj_F5J9LOapQzQ',
+    accountId: '5WXCTb4serGkI7g4Jf3hNMGFdGQ5_rfpg5WDCnDTBgQwNv_l5F6g7L9q',
+    puuid: 'lSrz9gPSxdQjDNpgbpdEnzTMKV2A8_xT5QZtx3wF0w8ydIogd-Q9aJwuh8YjG6dJ84iM2H3a5QhlmA',
+    name: 'oye papi',
+    twitter: "https://twitter.com/yonnareiss",
+    twitch: "https://www.twitch.tv/yonnareiss"
+},
+{
+    id: 'LLEbk3Z6na8pGnv0aK2ISU6VU_dTott_3Pbx09hUDp8J-_b0RKG6gjIL8w',
+    accountId: 'qiPipLPX16BdW-a4mRu-iiF73yFmdvZvuLkpKOGioepHfXCkmLuv4oeW',
+    puuid: 'RRBWPn00PLphhcJbrW4pypnhzjR25izQsY1bc78hOOxNOoRKSA1iapQYdcQmyznZJjxwY9Vgxt48ZQ',
+    name: '1Stream y al TFT',
+    twitter: "https://twitter.com/Reventxz",
+    twitch: "https://www.twitch.tv/ReventXz"
+},
+{
+    id: '2zKFkSbdwtrnH9mzs0wDAeUkfpehhtoCtv6OkHsAnKHPBLV4nANjX6COHA',
+    accountId: '6-3pYyYTA6NX5Win6k8sHm9gqzU-EFzs3GkQ20KJ5fL0gqcDgTQrUvse',
+    puuid: 'f965tTL9juUuJpjCWESF5sh0qDMxKv0xabImyha2c4xSWm9baaLwyHI_-OYR2T8mHNY9lHDdaXq21A',
+    name: 'Nyaesenar',
+    twitter: "https://twitter.com/Aesenar",
+    twitch: "https://www.twitch.tv/Aesenar"
+},
+{
+    id: 'A9jcta0h8ZiqV9iF1gKTtiiFJnho6QbwBuJzX2uy_XzpfCkdXEe2bi2ZWA',
+    accountId: 'N5uyaPbo4OWO7zKBw-WLMvmjlmUZCN0hcBIDhiZkZCbRyisfarbR2o8O',
+    puuid: '8T4wBzx5snZ_A4yd9lqBe9-MV-T_1f1t3yWvMOB-Y2mUXn0QdHUZFQdoc1oaHKPSpx9I5wI388N09w',
+    name: 'IG de la Marita',
+    twitter: "https://twitter.com/missPolaroid",
+    twitch: "https://www.twitch.tv/missPolaroid"
+},
+{
+    id: 'KSYNCkiksv_KVeXECg4trUUyuoZgMUsXNsUsH-Zn7EzBxKIb1Ktb68o7Jg',
+    accountId: 'pXIpCAM1AGQMzqFB5EV9C9dtiCCXAWNRvANUr3MUiWzobLk9yKPfqN8x',
+    puuid: 'CoAbv-DWy0EM-mw-Ak1yAYQqK8FKnW_MoQtUaCMfAzxraaL1Z7Lw_kStn6MG4kfhTVDD9rOZV0QV0A',
+    name: 'Perro de Elyoya',
+    twitter: "https://twitter.com/Supa_LoL",
+    twitch: "https://www.twitch.tv/supa_lol"
+},
+{
+    id: 'xRhPqvE8jGpNuOizpNd79IzA7CMqVy1VPFUiW6UZekJpM-9tykqVsdS4bQ',
+    accountId: 'iGGuHhpz04VF23HS6JkI4F1kmQXWudjeSslUa8L_Hw2ZRQmOIi_Nl6ww',
+    puuid: 'Qc-lDGZBjZEObGI7t9dNQYgCsVo-vwcbuVyj7FAaie6EJaHmyYZCVbcXTSUI3-udV0wbo7aTnyufIg',
+    name: 'CarmenSandwich',
+    twitter: "https://twitter.com/carmensandwich",
+    twitch: "https://www.twitch.tv/carmensandwich"
+},
+{
+    id: 'XQUMr73argSG1PdLIDq2MF5CLwqveLt7uYHacyY6X-2h_NhTZePtWmmiBw',
+    accountId: 'Yx-kfVI7EFYI8orVpk_h1It_6nglfTnUJi1YYT4hVQTnzGTWBXlKRKt_',
+    puuid: 'tuQPSQUx1PXOQr_tyfcv3xpE33_YrlN6akEgxh0XozzTR4YZ61RD-ea3HmMFlUeK8bylJNNPMFvKtA',
+    name: '2K4IN',
+    twitter: "https://twitter.com/SkainLoL",
+    twitch: "https://www.twitch.tv/SkainLoL"
+},
+{
+    id: 'v_e1crtNvMqRsvvrTvoEwFeHnngL3p8lIQObNSMpNfgJbrgRTuhxgR_3pg',
+    accountId: '6DAW91K6fHldQEbxVAO51BccD-RNy4OoJkVnYD5Qy1Y1QnODo45Nbn7I',
+    puuid: 'YsWAM1QBpu1iG0FBEnIIoOx7sd-5NRFLagdBqVIqbXxjQpNLrtA2OuN7nMdZqZorOzuJPn8bPId5rw',
+    name: 'rank 22',
+    twitter: "https://twitter.com/ffaka_",
+    twitch: "https://www.twitch.tv/ffaka"
+},
+{
+    id: 'gCpTh5nrU-Gh1jrhAVRq6PT6_BRnk6wFhCGeJo3gLH4FoYeJ8dzaKiVaBg',
+    accountId: 'tL1CRdCn3cdHpV8aPqzKbCXDiI1ocpAfEkY5MVolaLo0ykJvu9-WveZ8',
+    puuid: 'Xj3nukVcf4vwFkhAaBTdxp-bkUuxg45TCbxO3gaRvE2_gahA8WOU7GCIK24kuFCAwmD3gm4uLv6r_w',
+    name: 'BudinKos',
+    twitter: "https://twitter.com/InKoslol",
+    twitch: "https://www.twitch.tv/InKoslol"
+},
+{
+    id: 'Oy5CSF9HSC8lqRpmNxHR3uyv6qwwHJM-3CStt3NBwHwQbZBhObbvMet1Cw',
+    accountId: '4ppw_gB5HeR5vI1z69LvJv3poNVoBMRWsHTov88ncDVx0noIRTQosWSd',
+    puuid: 'S3t0o4XauAzn2FG9OqgtUH_srxMRxfuu3zVew-6TBzH3oVqCKYgLDl9oIwOp1CE97N9Gr--rp2cJ3A',
+    name: 'Filho Da Jungla',
+    twitter: "https://twitter.com/RafaL0L",
+    twitch: "https://www.twitch.tv/rafal0l"
+},
+{
+    id: 'eKAEU99lBlfBxKqpwoelvkVtgLnvpmrFQ2buWfI_RT5nEeJA3sMk0VdP6g',
+    accountId: 'VWPHezVKcGvNGtClL2-Cm32O1MiBWOmPeiP3BkiYD55mq-0Fb28EmByk',
+    puuid: 'ZoGQYH31wATfqX3bHuWuesdv98Dqq67hoVOXSpl3VKJpceB41Tl7ANrcj5s0SwOUStHtlYXBM2XAUQ',
+    name: 'DJ CUC4R4CH4',
+    twitter: "https://twitter.com/MinidukeLoL",
+    twitch: "https://www.twitch.tv/Miniduke"
+},
+{
+    id: '9hgToa99O9JLAVZ1JESNpDB6mOXkO-K8GODpXc20LoW_WLftddDlusWZvg',
+    accountId: 'BYcUAZmUtXosB9Hr8EEryW8U0Hsj75WJ-GxdHVtCJpAq_BfwYZYYftUi',
+    puuid: 'FaVw6LkRoZfnw4Bby_SETI82KwJeJEQdJDXIiKjteB23oxlbeOHkL9tLK7ioNNeIza2iWe7gcOZdqQ',
+    name: 'El Yayo Retirado',
+    twitter: "https://twitter.com/IamDiamondLoL",
+    twitch: "https://www.twitch.tv/iamdiamond"
+},
+{
+    id: 'tlcXdSDzqy1-JAJqm137-hHz2NYv7AfXJEPeCv5v-2NGXN5E9XG7nTJ01Q',
+    accountId: 'NB9CBLo8LdCJy5eX7H8p8rZMw-Kfwq_kheIfkmT6VtjCB11ajhICHwc1',
+    puuid: '7u3Hvu6CDH6w9rZ9GxIX2srnE4gjeAUc0o-yPrUCSpwHu5XUxHTmqNZHGmMHO1AHtIvizKpunBdZZw',
+    name: 'PELUK1NG',
+    twitter: "https://twitter.com/Th3AntonioGG",
+    twitch: "https://www.twitch.tv/Th3Antonio"
+},
+{
+    id: 'XcbR6dTenIKkDrOPL0vlweeH0rvpqQ1vFxV5ryfH-Q0snXkMVm6mzpOgjg',
+    accountId: 'C5k9LGuscvvIv167pn-p3x3LlN4H6ehG1v-wvB2ZdOK7b9P7apJXRHk-',
+    puuid: 'EKp26JV6v1aSb6KZ25uRlshsEsrGSnpbdz8kVnmR8V1ZR4sqCqFto4MVNyWSPFzIe1s1rWPokS4vZA',
+    name: 'ElHoyoNinja',
+    twitter: "https://twitter.com/ElOjoNinja",
+    twitch: "https://www.twitch.tv/ElOjoNinja"
+},
+{
+    id: 'y_EORz6cQGdiKi3RL2ZpUGxqg0KaT1KIiWpj4L8OHbOev0uNbQOEKJC7qA',
+    accountId: 'a9hjMYZ9H2WKHVYjwIGcdPWBToS7hzFoUa08muwzD2CGBfEbJts0wRVN',
+    puuid: '4eESGR2_gEOTfukaDxbsU5TqPGIxW0KLGIsicFC_LHuTpYIytaIV8pJG-4ceZmfKz2jo6_y9RqC42A',
+    name: 'Islas Malvinas',
+    twitter: "https://twitter.com/NeckoLol",
+    twitch: "https://www.twitch.tv/NeckoLol"
+},
+{
+    id: 'k79ccsbqEl-yCGkuOOD-jloU70M-yEWjAS9aBaPszbpSHEzTBDWiGBlKjw',
+    accountId: 'vci8nCjmtVtEONTFGj5sSzgezBP1ToBtNMKwcBypwnTS8hxXRGXAJx1e',
+    puuid: 'bY_zvb3eQ-cgvWlEvTDUVx0zcvO0a20M8MTs-kNxJE9zsaVy2nWBB0_ioCB5otEACG_sDC133EWl6Q',
+    name: 'Diamond Caster',
+    twitter: "https://twitter.com/Champi_14",
+    twitch: "https://www.twitch.tv/champi14"
+},
+{
+    id: 'lQgctUIOuou8u9s_bqkSrK0fz_k9jzZOJH7j5iL0I8imhm3LIAyBmJVijg',
+    accountId: 'HpxzWuyF9K5rBeO3K4rHBNNlZpi2u6290XLfN1UdSVH_zQV2OMYlbYjz',
+    puuid: 'X05ukU_quADsZShHNLRRvlTrvd7dHxybuRUlpqRg7aFpobMP2thkbbOaIxvacDhRBCbVGYl3iK7XQg',
+    name: 'Pause and Play',
+    twitter: "https://twitter.com/PauSenpai",
+    twitch: "https://www.twitch.tv/pausenpaii"
+},
+{
+    id: 'dImEW9ja6NnJilGJ9r4j1kQUnfmM-FI30vtgvJuA2exqhEA3ZJXaqj5wiA',
+    accountId: 'SqG57hvnqh7q_b5-UHFiwe7Bi7xUP61xYNYxZm8K9GD6mpy6JiSI63Kf',
+    puuid: 'aKag5T9mf8BIYhAzDOps_zK168ofa3-RDlh4lwTItJ-OmamVOxbWoGBCAf3DEYmN58IwI7sDJnSy-g',
+    name: 'Isolation Sallan',
+    twitter: "https://twitter.com/Sallanman_cat",
+    twitch: "https://www.twitch.tv/Sallanman_cat"
+},
+{
+    id: 'VI5yciUa0AT1hu_7NhnaeYfocp2H6D30HQJzQRZncJk0-t3G58YpX6QRZQ',
+    accountId: 'Yvrn5DNsui9hwSezAyDRoviGD_0rtvi8upYzkTEbVcqIoTj3Dd7M-YIX',
+    puuid: 'qJQvMmwugBTrKqdz8y3aU6xES5sqoZ5hSqeJABoEVP6pcBy5D2sfb3Q-TOIYucM8jS_Kh5ri3cLaRg',
+    name: 'Jayce la compra',
+    twitter: "https://twitter.com/holdemhammers",
+    twitch: "https://www.twitch.tv/holdemhammers"
+},
+{
+    id: 'YtIx79DdQ7cDnz5MDQEieQ1Fpaj5KcBuuuxkfYUekBLzmsGl-landRXUpg',
+    accountId: 'kZvIg0K8LnZ9HRUIYxt9r4V4zj4O1RTH8E3gErSgcZwroBi_fWzvHdxh',
+    puuid: 'Qj9pgBLhDUpIh0dfoUcMh0wVygohfh9YrG8QddETT4mpnQ5cZ2ouzYZcR0kGxVaQgspZehfbRjOQQw',
+    name: 'Hot Tub Player',
+    twitter: "https://twitter.com/KatthTV",
+    twitch: "https://www.twitch.tv/Katth"
+},
+{
+    id: 'cVocaLYSu8tjJfLllfRsz9kBd5Nn8znorwTLAvTxTUcvbyES7lzn0vvFoA',
+    accountId: 'HXsdnB2tmpKqtfHPk3iGtOnjtFfEQUHJbMsM0ygDv77mCTQDi8Lul4ej',
+    puuid: 'Niow51XY-R6IvO2xlzewjscC_f3NOwvYbA2FYt1XyTFH4MCAILw1atF9kPnY4WLac2rVUl4GEHXU4A',
+    name: 'Laqtecuento',
+    twitter: "https://twitter.com/ZeIing",
+    twitch: "https://www.twitch.tv/zeling"
+},
+{
+    id: 'ErP3CbsOefg2B0dgXq933x4cSqWG7-0OEOpmvaP97ciXyIqL2PEH41wyDQ',
+    accountId: '8QD39cNdHM2iCxRmH1-DET6HnSPgxrLiP1D2ea_W_2KnLweeSXGSPUOn',
+    puuid: 'F3onG3b-Pcu1sf-5obFuMWxKljvnBqWh_QlWXsj9cfSpapteNJfXFDxt6ggk7gIr5AmrFjju_zLDDQ',
+    name: 'Madres Enjoyer',
+    twitter: "https://twitter.com/Send0o",
+    twitch: "https://www.twitch.tv/send0o"
+},
+{
+    id: 'F185ZPvGc2IEqnfYAbR0OHpTaAbhPF-HKZEe70_QiELQG4H_U4e-g3-v4g',
+    accountId: 'p23C6SFz6_yFFkNOokXedK9FYJHXoiyyCXNNhwVymPSzPry2L4TMpsMp',
+    puuid: 'iy6tUL1ntMk60DijoQm0zl7OShW5Iu0GpKsXfs8rjaz6n2XtJb-D-9pBfzrdvg-yl4frMyKs3AnD7w',
+    name: 'El Olivillas',
+    twitter: "https://twitter.com/JaVaaaLoL",
+    twitch: "https://www.twitch.tv/javaaa1"
+},
+{
+    id: '25WP-4y3UzHn8-FBe1nqa3okIOZChHYlKbInPmqcVaIkgS6yfBhQfZs9sA',
+    accountId: 'xgfJm9KdIftBQgUlEsejRgzeUYFttQRYVYGho4d8tpy9zuUyLNZnU3a9',
+    puuid: 'FDpje_FE1wGfxRmXeOBEjI8dzkZ6lK9vxFYGdcB2skADAxLxMd73iUkQ1BjOwpqr3fmZEoYqSHN6tQ',
+    name: 'Kulerios',
+    twitter: "https://twitter.com/KeriosYT",
+    twitch: "https://www.twitch.tv/kerios"
+},
+{
+    id: '78uMlWydUT3eikINLfpC6dTzIZp2Xaxi8_o9yyj9uKpXsfEiREWhqZvQrw',
+    accountId: 'SLa1QE4ZqeSkEwUe_2rYWfHm058Gn3eCzzmu8A7rQ6ALn-5s5FZZ9l6C',
+    puuid: 'VZGKlUot0ZFE5O1sx80Fksn2z9EnL8te0VA43QND-oNoQXfY28QVdYlo_KeL0SL9ph5JOaALRAQJJQ',
+    name: 'Xixarras',
+    twitter: "https://twitter.com/Xixauxas1",
+    twitch: "https://www.twitch.tv/xixauxas"
+},
+{
+    id: 'gP-1euQbPZ_Plp3WKxt__eaKCmeGcmNChDKisE3hdhntVmsTWA0hlUhmng',
+    accountId: 'G1o0-4zVefbMWd2hQ8Zh4CPP8GHn1JtYCnY5R2FMtuITIda4l4ocZDDQ',
+    puuid: 'RhmI4E9p01K5ukvsqE-WKURA_VZtvxcZCNzaXhJvPWy80Hfl-Nr4ctiLW8bvIMK8cYDVm6HU20WYjg',
+    name: 'George Casanovas',
+    twitter: "https://twitter.com/Werlyb",
+    twitch: "https://www.twitch.tv/Werlyb"
+},
+{
+    id: 'PZbi0Jn5dqNrV-BoBLpH97Ns1CTfODzhaHPriwLTWel5rNLmKnGtVEy-6w',
+    accountId: 'ubMQpohEpH4OAnZ9s0sfefyG9X65sqAIuxYZopkKFk5z02zBfltP3T0k',
+    puuid: 'NBsmAdcfXlEH2X739UMzZ906Q0a84Pzzg5h7B34seLhdtl_ZSmAx4OGuiaJtgAsocs9YwZGoyzyJhA',
+    name: 'Calvenger',
+    twitter: "https://twitter.com/Adri_Dalvenger",
+    twitch: "https://www.twitch.tv/adcshiiro"
+},
+{
+    id: 'cFZu3rmDpjs5MrzHM1w1f68ZB8Zmp3eZJIlNPiCWjpUd5DFPz3kNVIbxGw',
+    accountId: '2en26hI23bSbqEE2LkGia-offoaK17_rPrMY1z6yr4RDrWdw5HxcCKYZ',
+    puuid: 'Ku0rgE2-uw17_Gq38-qqYp2Y-vIslpcQZgNTGI8NFuuBFf8kZOsoH2iFRhXkvxkknsi2oeTh9D3wzw',
+    name: 'Loser Queuebab',
+    twitter: "https://twitter.com/frizko94",
+    twitch: "https://www.twitch.tv/frizko94"
+},
+{
+    id: "l9arro437YoNMzeeSXwYTXGv9vDMi7942Wtey_gUJat-1G6TukfhyTOVpw",
+    accountId: "bIYL9TYAW4rBFQKfD9xaAHd9vsDESm9otXNa9yKrnfpd1vath-4bzgD-",
+    puuid: "S8fIs54tkNkQg9fmqFQYYIN4OQzqx-r9OsiDUNxVeh2EKwfAsqkXUTGzLP73L7Cwh_BspbXnz0AmYw",
+    name: "Prekøz",
+    twitter: "https://twitter.com/MiyuLoL",
+    twitch: "https://www.twitch.tv/miyugodvelk"
+},
+{
+    id: "lwXHoqhdP9jH8nLt8rdjFQae83pljpT2BmQb3L2_nDPWlNBalYkFGqpbWA",
+    accountId: "PZxDlTPk5pqwcC4L034I3bZ_fSFea8x49jCOQCABKohv8EeHMTyO8mM6",
+    puuid: "ibByDNVbozOWMGoFia5Tx6o0SOhc07A8VhiBOHZcCn1KXh60gEXDoU6V_DGDlw4xzOjieYo9PjBWzw",
+    name: "Elyøya Fangirl",
+    twitter: "https://twitter.com/ElmiilloR",
+    twitch: "https://www.twitch.tv/ElmiilloR"
+},
+{
+    id: "klgr5Vg7qxBWR_SQZRL0TfBfdxqKGaWfwUpHGEo-SwHs-3IWDJXD_OyJAQ",
+    accountId: "goVDfnErBuMS_Jbx1emmJrFZ-uW2XtDjgxQoo39sNG_ZRAMTRbUwX3Zg",
+    puuid: "oXTk6DjQghp4bxEr6bJ0BQjYP0XEmgYjjeC89W62qmVylUIgZKg_jAGZJrEdMgYI4-L-bvLkpMD7Lg",
+    name: "Bøttøm Feeder",
+    twitter: "https://twitter.com/mcadryh",
+    twitch: "https://www.twitch.tv/mcadryh"
+},
+{
+    id: "XU3GZMDAG7Ku3OHwgM4C2HDMP0GQZ1Bg7tbvOeMnG13aI1QVV-l6x-i6Pg",
+    accountId: "TnFdXgZHsGgN6jclpVqWiW57HFBU4eJgzJ53XClK8chD0NixEqQ1vNEk",
+    puuid: "ebQw1p8UDze1Yo2JYZHQlWMI6OD5DRc41BOg4oQKFVPlY6xADH3ZACVQwaNQwS28pa0ujcOKhRTKvA",
+    name: "LcƒrMrnngstr",
+    twitter: "https://twitter.com/DahVysGG",
+    twitch: "https://www.twitch.tv/dahvys"
+},
+{
+    id: "akmscvNluamEvHoyIxRTo9OIBJ-UlNI5izRDuPhXz6MBcYuxm_knn36wUA",
+    accountId: "GYxyzG2yh6rUMOMfYRoatJENSWpyksCeD_iE32Jgh5OKG2K95FnE8bZs",
+    puuid: "L3FPq8bRp1SqgpDsTIggBs-P4EVIzbOc__vkIxmHPIX8XDUL4bkZUZIF0ahmWHOzAErugKDe5kLsmQ",
+    name: "RøaD2ban",
+    twitter: "https://twitter.com/AdcShiiro",
+    twitch: "https://www.twitch.tv/adcshiiro"
+},
+{
+    id: "edTk9RChEb2cejEbH2rtiKP0OdKdVTlrEqAR0fp29MBKyq8SY6yOhjDKDg",
+    accountId: "86sXddbBF1xTGlCteqIxoLAdWVfFmfyR1x0p1QPiwil-kJBFKPtQnjlR",
+    puuid: "nZyh5hcIu3fBj3VBos9uPYowcfk59SNpnAb9C5ATX9BAm08t5gB0gzpoZ_1edqoncFkIr_Xwr4i7bg",
+    name: "Castañaman",
+    twitter: "https://twitter.com/SilerLol",
+    twitch: "https://www.twitch.tv/silerr8"
+},
+{
+    id: "91Up6DyBMFrgxmc8xJ9ne9KKggnwM2sPYgRl3EYRf95QfjNv9snFPYuaVg",
+    accountId: "pUGEG0cN2-8M6YB51wa7Z9SKy-eE-g0NQNjljvMPvheCLO-5qzJYbRZM",
+    puuid: "AQ1O_iiD6jyWRaJFmOFFlcJuze-Ziw-8e0IFzP0gZ4R7xijACFcmY0iu4-GT8ask-wmktw1mkiiI7w",
+    name: "Holandés Errante",
+    twitter: "https://twitter.com/KarakalJr",
+    twitch: "https://www.twitch.tv/KarakalJr"
+}
 ]
 
 const streamersUsername = streamers.map(str => {
